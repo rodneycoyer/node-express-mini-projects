491d079047072951c00917ec0453783063f66db0
roofWalker <roofwalker13@outlook.com>
2021-08-13 17:01:16 -0700

    Home, About, Err routes

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-    console.log('you found the server')
-    res.end('home page')
+    // console.log(req.method)
+    const url = req.url;
+    // home page
+    if (url === '/') {
+        res.writeHead(200, {'content-type':'text/html'})
+        res.write('<h1>Home Page</h1>')
+        res.end()
+    }
+    // about page
+    else if (url === '/about') {
+        res.writeHead(200, {'content-type':'text/html'})
+        res.write('<h1>About Page</h1>')
+        res.end()
+    }
+    // 404
+    else {
+        res.writeHead(404, {'content-type' : 'text/html'})
+        res.write('<h1>404 oops... page not found</h1>')
+        res.end()
+    }
 })
 
 server.listen(5000)
