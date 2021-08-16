4cb3ef26eec8f2c50ba9131fe35279caf52493d7
roofWalker <roofwalker13@outlook.com>
2021-08-16 16:57:48 -0700

    POST from browser with JSON

--- app.js
+++ app.js
@@ -4,11 +4,22 @@ let { people } = require('./data');
 
 // static assets 
 app.use(express.static('./methods-public'))
-// parse body for data
+// parse body form data
 app.use(express.urlencoded({ extended: false }))
+// parse json data
+app.use(express.json())
+
 
 app.get('/api/people', (req, res) => {
     res.status(200).json({ success : true, data : people })
+})
+
+app.post('/api/people', (req, res) => {
+    const { name } = req.body
+    if (!name) {
+        res.status(400).json({success : false, msg : 'please provide name value'})
+    }
+    res.status(201).send({ success: true, person : name })
 })
 
 app.post('/login', (req, res) => {
