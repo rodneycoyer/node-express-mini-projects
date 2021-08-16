84114a5841279e1d60fe866e11cac76cb4b0c2e6
roofWalker <roofwalker13@outlook.com>
2021-08-16 14:07:07 -0700

    api and query

--- app.js
+++ app.js
@@ -1,16 +1,55 @@
 const express = require('express');
-const path = require('path');
 const app = express();
+const { products } = require('./data');
 
-app.use(express.static('./public'))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+    res.send('<h1> Home Page </h1><a href="/api/products">products</a>')
 })
 
-app.all('*', (req, res) => {
-    res.status(404).send('resource not found')
-});
+app.get('/api/products', (req, res) => {
+    const newProducts = products.map((product) => {
+        const { id, name, image } = product
+        return { id, name, image }
+    })
+
+    res.json(newProducts)
+})
+
+app.get('/api/products/:productID', (req, res) => {
+    // console.log(req)
+    // console.log(req.params)
+    const { productID } = req.params
+    const singleProduct = products.find(
+        (product) => product.id === Number(productID)
+    )
+    if (!singleProduct) {
+        return res.status(404).send('product does not exist')
+    }
+    if (sortedProducts < 1 ) {
+        // res.status(200).send('no products match your search')
+        res.status(200).json({success : true, data : []})
+    }
+    // console.log(singleProduct)
+    res.json(singleProduct)
+})
+
+app.get('/api/v1/query', (req, res) => {
+    // console.log(req.query)
+    const { search, limit } = req.query
+    let sortedProducts = [...products]
+
+    if (search) {
+        sortedProducts = sortedProducts.filter((product) => {
+            return product.name.startsWith(search)
+        })
+    }
+
+    if (limit) {
+        sortedProducts = sortedProducts.slice(0, Number(limit))
+    }
+    res.status(200).json(sortedProducts)
+})
 
 app.listen(5000, () => {
     console.log(`server is listening on port 5000`)
